4d5fa636b08a8035241a456bba7d90c954bfee9a
maksim beloglazov <maksimbel20170@gmail.com>
2025-04-05 16:28:24 +0300

    обновление

--- report/MainWindow.xaml.cs
+++ report/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,8 +17,37 @@ namespace report;
 /// </summary>
 public partial class MainWindow : Window
 {
+    public class PriceData
+    {
+        public required string PriceCategory { get; set; }
+        public required string Price { get; set; }
+        public string? Tag { get; set; }
+    }
+    
+    private ObservableCollection<PriceData> prices = new ();
+    
     public MainWindow()
     {
         InitializeComponent();
+
+        List<PriceData> datas = new()
+        {
+            new PriceData { PriceCategory = "Ценовая категория 1", Price = "100"},
+            new PriceData { PriceCategory = "Ценовая категория 2", Price = "125"},
+            new PriceData { PriceCategory = "Ценовая категория 3", Price = "99", Tag = "Выгоднее всего"},
+            new PriceData { PriceCategory = "Ценовая категория 4", Price = "130"},
+            new PriceData { PriceCategory = "Ценовая категория 5", Price = "170"},
+            new PriceData { PriceCategory = "Ценовая категория 6", Price = "199"},
+        };
+
+        AddPriceData(datas);
+    }
+    
+    public void AddPriceData(List<PriceData> priceData)
+    {
+        foreach (var data in priceData)
+            prices.Add(new PriceData { PriceCategory = data.PriceCategory, Price = data.Price + '\u20bd', Tag = data.Tag });
+        
+        PriceCategory.ItemsSource = prices;
     }
 }
